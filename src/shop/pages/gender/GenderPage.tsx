49bd10818e406de052879836ae70d73f2a447eaa
vCentDev <vicentDev@yahoo.com>
2026-03-04 07:10:31 +0100

    Adds gender-specific product listing page to shop

--- src/shop/pages/gender/GenderPage.tsx
+++ src/shop/pages/gender/GenderPage.tsx
@@ -1,6 +1,26 @@
+import { CustomPagination } from "@/components/custom/CustomPagination"
+import { products } from "@/mocks/products.mock"
+import { CustomJumbotron } from "@/shop/components/CustomJumbotron"
+import { ProductsGrid } from "@/shop/components/ProductsGrid"
+import { useParams } from "react-router"
 
 export const GenderPage = () => {
+
+    const { gender } = useParams()
+
+    const genderLabel = gender === 'men'
+        ? 'Hombres'
+        : gender === 'woman'
+            ? 'Mujeres'
+            : 'Niños'
+
     return (
-        <div>GenderPage</div>
+        <>
+            <CustomJumbotron title={`Productos para ${genderLabel}`} />
+
+            <ProductsGrid products={products} />
+
+            <CustomPagination totalPages={7} />
+        </>
     )
 }
