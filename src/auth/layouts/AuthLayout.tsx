6bea491f2f568a4ad77bbb2dfad622b572b5b2be
vCentDev <vicentDev@yahoo.com>
2026-03-04 07:10:31 +0100

    Implements core authentication pages and layout

--- src/auth/layouts/AuthLayout.tsx
+++ src/auth/layouts/AuthLayout.tsx
@@ -1,7 +1,12 @@
+import { Outlet } from "react-router"
 
 export const AuthLayout = () => {
     return (
-        <div>AuthLayout</div>
+        <div className="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
+            <div className="w-full max-w-sm md:max-w-3xl">
+                <Outlet />
+            </div>
+        </div>
     )
 }
 
