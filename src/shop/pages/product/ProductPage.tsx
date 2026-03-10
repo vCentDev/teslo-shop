aa27ae3274307a9e410f8a448086c64e824e1e48
vCentDev <vicentDev@yahoo.com>
2026-03-10 22:03:57 +0100

    Refines product page layout

--- src/shop/pages/product/ProductPage.tsx
+++ src/shop/pages/product/ProductPage.tsx
@@ -1,6 +1,9 @@
 
 export const ProductPage = () => {
+
     return (
-        <div>ProductPage</div>
+        <div className="text-center">
+            <h1 className="mt-5 text-3xl font-montserrat">Product Page</h1>
+        </div>
     )
 }
