919d744a1bc1f1bdf957426d349035f2a147b1ec
vCentDev <vicentDev@yahoo.com>
2026-03-10 22:03:56 +0100

    Shows user info in admin sidebar

--- src/admin/components/AdminSidebar.tsx
+++ src/admin/components/AdminSidebar.tsx
@@ -1,3 +1,4 @@
+import { useAuthState } from '@/auth/store/auth.store';
 import { CustomLogo } from '@/components/custom/CustomLogo';
 import {
     Home,
@@ -20,6 +21,7 @@ interface Props {
 
 
 export const AdminSidebar = ({ isCollapsed, onToggle }: Props) => {
+    const { user } = useAuthState()
 
     const { pathname } = useLocation()
 
@@ -86,11 +88,11 @@ export const AdminSidebar = ({ isCollapsed, onToggle }: Props) => {
                 <div className="p-4 border-t border-gray-200">
                     <div className="flex items-center space-x-3 p-3 rounded-lg hover:bg-gray-50 transition-colors cursor-pointer">
                         <div className="w-10 h-10 bg-linear-to-br from-blue-500 to-purple-600 rounded-full flex items-center justify-center text-white font-semibold">
-                            JD
+                            {user?.fullName.split(" ").map((initial) => initial.charAt(0))}
                         </div>
                         <div className="flex-1 min-w-0">
-                            <p className="text-sm font-medium text-gray-900 truncate">John Doe</p>
-                            <p className="text-xs text-gray-500 truncate">john@company.com</p>
+                            <p className="text-sm font-medium text-gray-900 truncate">{user?.fullName}</p>
+                            <p className="text-xs text-gray-500 truncate">{user?.email}</p>
                         </div>
                     </div>
                 </div>
