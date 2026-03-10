ca1e56fe544b6db6473863ae050d4cb965516b08
vCentDev <vicentDev@yahoo.com>
2026-03-10 06:11:43 +0100

    Implements comprehensive product filtering

--- src/shop/actions/get-products.action.ts
+++ src/shop/actions/get-products.action.ts
@@ -6,17 +6,27 @@ const BASE_URL = import.meta.env.VITE_API_URL;
 interface Options {
   limit?: number | string;
   offset?: number | string;
+  sizes?: string;
+  gender?: string;
+  minPrice?: number;
+  maxPrice?: number;
+  query?: string;
 }
 
 export const getProductAction = async (
   options: Options,
 ): Promise<ProductsResponse> => {
-  const { limit, offset } = options;
+  const { limit, offset, gender, sizes, minPrice, maxPrice, query } = options;
 
   const { data } = await tesloApi.get<ProductsResponse>('/products', {
     params: {
       limit,
       offset,
+      sizes,
+      gender,
+      minPrice,
+      maxPrice,
+      q: query,
     },
   });
 
