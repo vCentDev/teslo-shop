be3770e332eed6a3628eebca443e2cb6fa9eb3d5
vCentDev <vicentDev@yahoo.com>
2026-03-10 22:03:55 +0100

    Applies route protection to auth and admin paths

--- src/app.router.tsx
+++ src/app.router.tsx
@@ -10,6 +10,7 @@ import { RegisterPage } from "./auth/pages/register/RegisterPage";
 import { AdminProductsPage } from "./admin/pages/products/AdminProductsPage";
 import { AdminProductPage } from "./admin/pages/product/AdminProductPage";
 import { DashboardPage } from "./admin/pages/dashboard/DashboardPage";
+import { AdminRoute, NotAuthenticatedRoute } from "./components/routes/ProtectedRoutes";
 
 
 const AuthLayout = lazy(() => import('./auth/layouts/AuthLayout'))
@@ -39,7 +40,11 @@ export const appRouter = createBrowserRouter([
     // Auth Routes
     {
         path: '/auth',
-        element: <AuthLayout />,
+        element: (
+            <NotAuthenticatedRoute>
+                <AuthLayout />
+            </NotAuthenticatedRoute>
+        ),
         children: [
             {
                 index: true,
@@ -59,7 +64,11 @@ export const appRouter = createBrowserRouter([
     // Admin Routes
     {
         path: '/admin',
-        element: <AdminLayout />,
+        element: (
+            <AdminRoute>
+                <AdminLayout />
+            </AdminRoute>
+        ),
         children: [
             {
                 index: true,
