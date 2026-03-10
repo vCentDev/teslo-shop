02952557f730b668e939f4203e9082d9f6cbb827
vCentDev <vicentDev@yahoo.com>
2026-03-10 09:32:18 +0100

    Add sonner library for toast notifications and update package dependencies

--- src/TesloShopApp.tsx
+++ src/TesloShopApp.tsx
@@ -2,12 +2,13 @@ import { RouterProvider } from "react-router"
 import { appRouter } from "./app.router"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools"
-
+import { Toaster } from 'sonner';
 const queryClient = new QueryClient()
 
 export const TesloShopApp = () => {
     return (
         <QueryClientProvider client={queryClient}>
+            <Toaster />
             <RouterProvider router={appRouter} />
             <ReactQueryDevtools initialIsOpen={false} />
         </QueryClientProvider>
